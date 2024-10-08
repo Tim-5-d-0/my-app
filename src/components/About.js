096e9e9a26580cbd0a56f705128acd6a48c0fed4
Tim-5-d-0 <timofeycheban644@gmail.com>
2024-10-08 10:34:22 +0300

    commit

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';  // Import the CSS file
+
+function About() {
+    return (
+        <section className="about-section">
+            <h2>About us</h2>
+            <p>The online store Beekeeping Garden offers a wide range of beekeeping products in retail, small and large wholesale with shipment to any region of Ukraine. Our couriers can deliver the goods in Mykolaiv (orders from 3000.00 UAH are delivered in Mykolaiv for free).</p>
+            <p>
+                There are several reasons why among the many stores, you should give preference to ours. First of all, these are favorable prices, convenient methods of payment and delivery. Also at us you have the opportunity to get not only the goods of the declared quality, but also a qualified consultation. Professionals with experience will always help with the choice, as they not only know about the characteristics of inventory or other goods, but also have experience in their practical application. This means that by choosing the services of our company, you will find the products that will best suit your situation.</p>
+
+                <p>Our specialist beekeeper with experience will help you to select plant seeds to create a honey conveyor belt in your region for the whole season, to avoid failures in the work of bees during the bee-free period.</p>
+
+                <p>Specialists of the online store follow the novelties in the field of beekeeping, so you can always find modern bee equipment.</p>
+
+            <p>We are approached by beginners who are setting up their business, experienced beekeepers who prefer to buy goods in small quantities, as well as retail stores specializing in beekeeping products.</p>
+
+            <p>We welcome long-term cooperation, and provide favorable conditions for this. At discounts can also count on all who decided to buy from us a large batch of goods. Select products easily at any convenient time on the site, in the catalog you will find all the necessary information.</p>
+        </section>
+    );
+}
+
+export default About;
